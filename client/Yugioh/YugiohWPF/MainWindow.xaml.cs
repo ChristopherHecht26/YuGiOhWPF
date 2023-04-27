ca48ba5be182a6b266d13a907ffb7cc39acbbf47
FabiusPaulus <benthschneiderfabian@gmail.com>
2023-04-27 07:51:46 +0200

    neueste version

--- client/Yugioh/YugiohWPF/MainWindow.xaml.cs
+++ client/Yugioh/YugiohWPF/MainWindow.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Net.Http;
 
 namespace YugiohWPF
 {
@@ -29,52 +28,88 @@ namespace YugiohWPF
         private int card_count; //zählt die menge hinzugefügter Karten
 
         private int col_count; //zählt columns, um einen rechtzeitigen Umbruch einzufügen
+        private int col_position_max; //zählt columns, sobald die 15 Spalten eingefügt sind
         private int row_position = 0; //zeigt row postion (cursor)
 
         ColumnDefinition gridCol = new ColumnDefinition();
-        API api = new API();
+        RowDefinition rowCol = new RowDefinition();
 
         private void add_card_to_deck()
         {
 
             //Erstellt eine "Karte" --> das hier muss zu einer echten Karte geändert werden (Label ist Platzalter)
-            Label card1 = new Label();
-            card1.Content = "Karte";
+            TextBlock card1 = new TextBlock();
+            card1.Text = "Karte" + card_count.ToString();
             card1.FontSize = 14;
             card1.FontWeight = FontWeights.Bold;
+            card1.Background = Brushes.Beige;
 
             //Karten setzen
-            if(col_count < 15)
+            if(col_count == 10)
             {
-                grd_cardholder.ColumnDefinitions.Add(new ColumnDefinition()); //erstellt eine neue dynamische Spalte
-                Grid.SetRow(card1, row_position);
-                Grid.SetColumn(card1, col_count);
-                grd_cardholder.Children.Add(card1);
-                col_count++;
+                col_position_max++;
+                if(col_position_max == 9)
+                {
+                    Grid.SetRow(card1, row_position);
+                    Grid.SetColumn(card1, col_position_max);
+                    grd_cardholder.Children.Add(card1);
+                    card_count++;
+                }
+
+                if(col_position_max == 10)
+                {
+                    grd_cardholder.RowDefinitions.Add(new RowDefinition()); // erstellt dynamische reihe
+                    row_position++;
+                    col_position_max = 0;
+                }
+              
             }
-            if(col_count == 16)
+            if(col_count < 10)
             {
-                grd_cardholder.RowDefinitions.Add(new RowDefinition());
-                row_position++;
-                Grid.SetRow(card1, 1);
-                Grid.SetColumn(card1, col_count);
-                grd_cardholder.Children.Add(card1);
-                col_count = 0;
+                if(col_count == 0)
+                {
+                    grd_cardholder.RowDefinitions.Add(new RowDefinition()); // erstellt dynamische reihe
+                    grd_cardholder.ColumnDefinitions.Add(new ColumnDefinition()); //erstellt eine neue dynamische Spalte
+                    col_count++;
+
+                    //Karten Count Hoch
+                    Grid.SetRow(card1, row_position);
+                    Grid.SetColumn(card1, col_position_max);
+                    grd_cardholder.Children.Add(card1);
+                    card_count++;
+                }
+                else 
+                {
+                    grd_cardholder.ColumnDefinitions.Add(new ColumnDefinition()); //erstellt eine neue dynamische Spalte
+                    col_count++;
+                    col_position_max++;
+                }
+                if(col_position_max == 9) 
+                {
+                    Grid.SetRow(card1, row_position);
+                    Grid.SetColumn(card1, col_position_max);
+                    grd_cardholder.Children.Add(card1);
+                    card_count++;
+                }
+                
             }
 
-            //Karten Count Hoch
-            card_count++;
-            lbl_Info.Content = "Deckinfo : " + card_count.ToString();
+            if(col_count > 1 & col_position_max != 9)
+            {
+                //Karten Count Hoch
+                Grid.SetRow(card1, row_position);
+                Grid.SetColumn(card1, col_position_max);
+                grd_cardholder.Children.Add(card1);
+                card_count++;
+            }
+            lbl_Info.Content = "Deckinfo :  Spaltencount : " + col_count.ToString() +"Spalte : " + col_position_max.ToString() + "reihen : " + grd_cardholder.RowDefinitions.Count.ToString();
+
 
         }
 
         private void btn_Create_Click(object sender, RoutedEventArgs e)
         {
             add_card_to_deck();
-            Filter filter = new Filter();
-            this.Visibility = Visibility.Hidden;
-            filter.Show();
         }
-
     }
 }
