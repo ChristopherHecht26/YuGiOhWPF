c65e2794e75792c2a5d15b532c2661cfcc647f49
unknown <User@K05-1046-P024.rbz-technik-kiel.de>
2023-05-04 10:42:53 +0200

    Filter

--- client/Yugioh/YugiohWPF/Filter.xaml.cs
+++ client/Yugioh/YugiohWPF/Filter.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,6 +23,42 @@ namespace YugiohWPF
         public Filter()
         {
             InitializeComponent();
+        }
+
+
+        string requestURI = "https://db.ygoprodeck.com/api/v7/cardinfo.php?";
+        string name;
+        HttpClient httpClient = new HttpClient();
+
+        private void txtBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //https://db.ygoprodeck.com/api/v7/cardinfo.php?fname=
+            name = "name=" + txtBoxSearch.Text;
+
+        }
+
+        private void cmbBox_CardType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void txtBoxATK_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtBoxDEF_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnSuchen_Click(object sender, RoutedEventArgs e)
+        {
+            requestURI = "https://db.ygoprodeck.com/api/v7/cardinfo.php?name=" + name + "&" + ;
+            HttpResponseMessage httpResponse = httpClient.GetAsync(requestURI).Result;
+
+            string response = httpResponse.Content.ReadAsStringAsync().Result;
+            txtBlockTest.Text = response;
         }
     }
 }
