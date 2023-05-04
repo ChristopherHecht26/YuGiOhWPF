329b8badb0c5cd663216fe071d67655a0247d2fb
unknown <User@K05-1046-P024.rbz-technik-kiel.de>
2023-05-04 10:05:58 +0200

    Filter angepassst

--- client/Yugioh/YugiohWPF/Filter.xaml.cs
+++ client/Yugioh/YugiohWPF/Filter.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,9 +20,48 @@ namespace YugiohWPF
     /// </summary>
     public partial class Filter : Window
     {
+        string requestURI = "";
+        string text;
         public Filter()
         {
             InitializeComponent();
+        }
+        HttpClient httpClient = new HttpClient();
+
+        private void txtBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //https://db.ygoprodeck.com/api/v7/cardinfo.php?fname=
+            text = txtBoxSearch.Text;
+            
+
+            //HttpResponseMessage httpResponse = httpClient.GetAsync(requestUri).Result;
+
+            //string response = httpResponse.Content.ReadAsStringAsync().Result;
+            //txtBlockTest.Text = response;
+        }
+
+        private void cmbBox_CardType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void txtBoxATK_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtBoxDEF_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnSuchen_Click(object sender, RoutedEventArgs e)
+        {
+            requestURI = "https://db.ygoprodeck.com/api/v7/cardinfo.php?name=" + text;
+            HttpResponseMessage httpResponse = httpClient.GetAsync(requestURI).Result;
+
+            string response = httpResponse.Content.ReadAsStringAsync().Result;
+            txtBlockTest.Text = response;
         }
     }
 }
